d65717f75a0007367af6326b2d67a68fed58d086
Alex <Alexok564@gmail.com>
2021-07-07 20:43:25 -0500

    py testing

---
+++ intro.py
@@ -0,0 +1,7 @@
+print('What is your name? ')
+userName = input()
+print('Hello ' + userName + ', nice to meet you.')
+print('When were you born? ')
+userAge = int(input())
+newAge = (2021-userAge)+1
+print('You will be '+ str(newAge)+ ' next year!')
